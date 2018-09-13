a1dcc61e43942e6dd9eb93a013463dfabaef7732
ebroadbent <ebroadbent@CLC-RM209-TWS.TCSD.tooelesd.org>
2018-09-13 11:41:49 -0600

    "test push

---
+++ src/Ericsb52/ch3examples/Assesment3_2.java
@@ -0,0 +1,15 @@
+package Ericsb52.ch3examples;
+
+public class Assesment3_2 {
+
+	public void main(string[] args) {
+		double i = 50.0;
+		double k = i+50.0;
+		double j = k+1;
+		System.out.println("J is "+ j +"and"
+				" k is " + k);
+		
+
+	}
+
+}
