020315b4ee36f1874e53c32b7291e00677522c21
ebroadbent <ebroadbent@CLC-RM209-TWS.TCSD.tooelesd.org>
2018-09-14 11:50:59 -0600

    small cleanup

--- src/Ericsb52/ch3examples/ComputeArea.java
+++ src/Ericsb52/ch3examples/ComputeArea.java
@@ -12,11 +12,11 @@ public class ComputeArea {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		// step one: enter the radius of a circle
+		
 		//constant declinations
 		double PI = 3.1415926535979323846264338327950288419716939;
 		
-		
+		// step one: enter the radius of a circle
 		double radius;
 		double area;
 		
@@ -24,16 +24,12 @@ public class ComputeArea {
 		System.out.println("enter the radius of a circle and press enter: ");
 		radius = input.nextDouble();
 		
-		
 		//step two: compute the area using the following formula:
 		// area = radius*radius*pi
-		
 		area = radius*radius*PI;
 		
-		
 		//step three: display result
-		
-		System.out.println("the area of your circle is:" + area +"\"");
+		System.out.println("after you entered the radius of " + radius + " We have computed the area of your circle as: " + area +"\"");
 
 	}
 
