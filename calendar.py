1146b28af6535ecc44b6ee96a7adb4f2d6f3d072
Eric Broadbent <Broadbent52@aol.com>
2018-09-21 10:57:00 -0600

    Add files via upload

---
+++ calendar.py
@@ -0,0 +1,20 @@
+import calendar as cal
+import time
+
+
+def current_time():
+    seconds = cal.timegm(time.gmtime())
+    current_seconds = seconds % 60
+    minutes = seconds // 60
+    current_minutes= minutes % 60
+    hours = minutes // 60
+    current_hour = hours % 24
+
+    return current_seconds, current_minutes, current_hour
+
+s,m,h = current_time()
+print(h,m,s)
+
+
+
+
---
+++ testclock.py
@@ -0,0 +1,4 @@
+import calendar
+import time
+x=calendar.timegm(time.gmtime())
+print(x)
